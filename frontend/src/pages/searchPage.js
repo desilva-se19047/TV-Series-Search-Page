aabcebcb680858b765fa7e52d44039ba1f371419
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2024-01-25 22:59:17 +0530

    add some files and codes

---
+++ frontend/src/pages/searchPage.js
@@ -0,0 +1,54 @@
+import React, { useState} from 'react';
+import './searchPage.css';
+import Fuse from 'fuse.js';
+const searchData = require("../resources/data.json");
+
+const options = {
+  keys: ['shows'],
+  includeScore: true, // Include score to rank results by relevance
+  threshold: 0.99, // Adjust the threshold based on preference
+};
+
+function SearchPage() {
+  const [value, setValue] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const onSearch = () => {
+    // Update search results based on the API response
+    const fuse = new Fuse(searchData.shows.map(show => ({ shows: show })), options);
+    const results = fuse.search(value).map(result => result.item.shows);
+    setSearchResults(results);
+  };
+
+  const onInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="search-page-container">
+      <div className="app-container">
+        <h1>Search TV Series</h1>
+      </div>
+      <div className="search-container">
+        <input type="text" onChange={onInputChange} value={value} />
+        <button onClick={onSearch}>Search</button>
+      </div>
+      <div className="results-container">
+        {searchResults.length > 0 && (
+          <div>
+            <h2>Results:</h2>
+            <ul>
+              {searchResults.slice(0, 3).map((show, index) => (
+                <div className="results" key={index}>
+                  {show}
+                </div>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default SearchPage;
